9f6d5141f73e3e59265ec637050d927831d9652a
Kramboja <koen5067@icloud.com>
2015-02-09 12:49:39 +0100

    Score added + started highscore

---
+++ Assets/Scripts/Resources.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Resources : MonoBehaviour {
+
+	private int _score;
+	private Score _scoreUI;
+	
+	void Awake()
+	{
+		_scoreUI = gameObject.GetComponent<Score>();
+	}
+
+	void Start()
+	{
+		_scoreUI.UpdateUI(_score);
+	}
+	
+	public int score()
+	{
+		return _score;
+	}
+	
+	public void SetScore(int amount)
+	{
+		_score = amount;
+	}
+	
+	public void UpdateScore(int amount)
+	{
+		_score += amount;
+	}
+}
---
+++ Assets/Scripts/UploadHighScore.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UploadHighScore : MonoBehaviour {
+
+	private bool isConnectedToInternet;
+
+	void Awake()
+	{
+		checkInternet();
+	}
+
+	public void checkInternet()
+	{
+		isConnectedToInternet = false;
+		
+		if (Network.player.ipAddress.ToString() != "0.0.0.0")
+		{
+			isConnectedToInternet = true;      
+		}
+
+		Debug.Log(isConnectedToInternet);
+	}
+
+	public void UploadScore()
+	{
+		if(isConnectedToInternet)
+		{
+			/*Script for uploading your score here*/
+		}
+		else
+			Debug.Log("Not connected to the internet...");
+	}
+
+
+}
