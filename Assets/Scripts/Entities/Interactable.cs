c838c622a13a0317066e06632d6c856eca2511de
Niek Schoone <16789@ma-web.nl>
2015-02-09 12:14:50 +0100

    Save/Loader + first try at fish

---
+++ Assets/Scripts/Entities/Interactable.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interactable : MonoBehaviour 
+{
+	protected float speed;
+	protected float timeCounter = 0;
+	
+	protected virtual void Start() 
+	{
+
+	}
+	
+	protected virtual void Update() 
+	{
+		Movement();
+	}
+
+	protected virtual void Movement()
+	{
+
+	}
+}
