1c21c2cc368c693b42e6251e1269310cddcd0da3
Kramboja <koen5067@icloud.com>
2015-02-09 11:54:14 +0100

    Time loopt door
    De time hoeft dus niet meer aangemaakt te worden voor iedere scene maar
    blijft bestaan. Iedere keer dat je een nieuwe scene opent reset hij de
    tijd weer.

---
+++ Assets/Scripts/TimeScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimeScript : MonoBehaviour {
+
+	private int _timeRemaining = 10;
+	private Text _timeRemainingText;
+
+	void Awake () 
+	{
+		DontDestroyOnLoad(transform.parent);
+		_timeRemainingText = GetComponent<Text> ();
+
+		startGame();
+	}
+
+	public void startGame()
+	{
+		_timeRemaining = 10;
+		InvokeRepeating ("timeDown", 1f, 1f);
+		_timeRemainingText.text =  "" + _timeRemaining;// + timeRemaining;	//timeRemaining.ToString();
+
+	}
+	
+	private void timeDown()
+	{
+		if(_timeRemaining == 0)
+		{
+			_timeRemainingText.text =  null;
+			CancelInvoke();
+			Application.LoadLevel("Shop");
+			Debug.Log("Died");
+		}
+		else
+		{
+			_timeRemaining --;
+			_timeRemainingText.text =  "" + _timeRemaining;
+		}
+		Debug.Log (_timeRemaining);
+	}
+
+	void OnLevelWasLoaded(int level) 
+	{
+		//prepare each scene for startup.
+		if(level != 1)
+		{
+			startGame();
+		}
+	}
+}
