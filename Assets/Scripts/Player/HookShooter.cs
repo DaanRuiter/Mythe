7768cceee8d82fdc004ef2d013857e0935f3e7ac
Daan Ruiter <daanruiter@live.nl>
2015-02-10 15:57:58 +0100

    Harpoon aiming, shooting and retracting.

---
+++ Assets/Scripts/Player/HookShooter.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HookShooter : MonoBehaviour {
+    private GameObject _harpoon;
+    private GameObject _harpoonGun;
+    private GameObject _targetLocator;
+
+    private HarpoonController _harpoonController;
+    private HarpoonAimer _harpoonAimer;
+
+    private LineRenderer _lineRenderer;
+
+    private bool _harpoonTraveling;
+
+    private void Start()
+    {
+        _harpoon = GameObject.FindGameObjectWithTag("Harpoon");
+        _harpoonGun = GameObject.FindGameObjectWithTag("HarpoonGun");
+        _targetLocator = GameObject.FindGameObjectWithTag("TargetLocator");
+        _harpoonController = _harpoon.GetComponent<HarpoonController>();
+        _harpoonAimer = transform.parent.GetComponent<HarpoonAimer>();
+        _lineRenderer = GetComponent<LineRenderer>();
+        _lineRenderer.SetPosition(0, transform.position);
+    }
+
+    private void Update()
+    {
+        _lineRenderer.SetPosition(1, _harpoon.transform.position);
+    }
+
+    public void ShootHarpoon()
+    {
+        _harpoon.transform.parent = null;
+        _harpoon.transform.rotation = HarpoonAimer.Lookat2D(_harpoon.transform, _targetLocator.transform);
+        _harpoonController.ShootAt(_targetLocator.transform.position);
+        _harpoonAimer.rotate = false;
+    }
+}
---
+++ Assets/Scripts/Player/TargetLocator.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetLocator : MonoBehaviour {
+
+	public Transform transformA;
+    public Transform transformB;  
+    public Vector3 bending = Vector3.up;                
+    public float timeToTravel = 10.0f;
+    public float directionSwithDistance;
+
+    private Vector3 _currentDestination;
+
+     private void Start () {
+         StartCoroutine(MoveToPosition(transformA.position, transformB.position));
+         _currentDestination = transformB.position;
+     }
+ 
+    private void Update()
+     {
+         if (Vector3.Distance(transform.position, _currentDestination) < directionSwithDistance)
+         {
+             SwitchDirection();
+         }
+     }
+
+    private IEnumerator MoveToPosition (Vector3 originPosition, Vector3 destination) {
+         float timeStamp = Time.time;
+         while (Time.time<timeStamp+timeToTravel) {
+
+             Vector3 currentPos = Vector3.zero;
+
+             currentPos = Vector3.Lerp(originPosition, destination, (Time.time - timeStamp) / timeToTravel);
+         
+             currentPos.x += bending.x*Mathf.Sin(Mathf.Clamp01((Time.time - timeStamp)/timeToTravel) * Mathf.PI);
+             currentPos.y += bending.y*Mathf.Sin(Mathf.Clamp01((Time.time - timeStamp)/timeToTravel) * Mathf.PI);
+             currentPos.z += bending.z*Mathf.Sin(Mathf.Clamp01((Time.time - timeStamp)/timeToTravel) * Mathf.PI);
+
+             transform.position = currentPos;
+
+             yield return null;
+         }
+    }
+
+    private void SwitchDirection()
+    {
+        if(_currentDestination == transformA.position)
+        {
+            _currentDestination = transformB.position;
+            StopAllCoroutines();
+            StartCoroutine(MoveToPosition(transformA.position, transformB.position));
+        }
+        else
+        {
+            _currentDestination = transformA.position;
+            StopAllCoroutines();
+            StartCoroutine(MoveToPosition(transformB.position, transformA.position));
+        }
+    }
+}
