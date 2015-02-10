9f7919a16b1a5becebdcf22b0fb6dcd9a440bdee
Niek Schoone <16789@ma-web.nl>
2015-02-10 17:00:36 +0100

    more fish AI more conventional
    Also added my own scene te work in

---
+++ Assets/Scripts/Entities/SmallFish_1.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmallFish_1 : FishPickup
+{
+	private float _angle;
+
+	void Start() 
+	{
+		fishPoints = 10;
+
+		speedMultiplier = 1;
+
+		radiusX = 5;
+		radiusY = 5;
+	}
+	
+	protected override void FixedUpdate () 
+	{
+		base.FixedUpdate();
+	}
+
+	protected override void Movement()
+	{
+		direction = new Vector2(x, y);
+
+		_angle = Mathf.Atan2(direction.y, direction.x) * (180 / Mathf.PI) + 90;
+
+		x = Mathf.Cos(mathSpeed) * radiusX;
+		y = Mathf.Sin(mathSpeed) * radiusY;
+		
+		transform.rotation = Quaternion.Euler(0, 0, _angle);
+
+		transform.position = direction;
+	}
+}
---
+++ Assets/Scripts/Entities/SmallFish_3.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmallFish_3 : FishPickup 
+{	
+
+	private bool _negativeMovement;
+
+	void Start() 
+	{
+		fishPoints = 10;
+		speedMultiplier = 0.5f;
+		
+		radiusX = transform.position.x + 5;
+
+		x = transform.position.x;
+		y = transform.position.y;
+
+		_negativeMovement = false;
+	}
+	
+	protected override void FixedUpdate () 
+	{
+		base.FixedUpdate();
+	}
+	
+	protected override void Movement()
+	{
+		direction = new Vector2 (x, y);
+		
+		if(transform.position.x >= radiusX)
+		{
+			_negativeMovement = true;
+		}else if(transform.position.x <= -radiusX)
+		{
+			_negativeMovement = false;
+		}
+		if(_negativeMovement)
+		{
+			x -= speed;
+		}else if(!_negativeMovement)
+		{
+			x += speed;
+		}
+		transform.position = direction;
+	}
+}
