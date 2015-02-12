3b776a9ff318ffe0660804c596ad740550dd3c69
Kramboja <koen5067@icloud.com>
2015-02-12 09:46:28 +0100

    ShopBase

---
+++ Assets/Scripts/UI/ShopScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShopScript : MonoBehaviour {
+
+	public GameObject[] canvas;
+	private int lastScreen;
+
+	void Awake()
+	{
+		lastScreen = 0;
+	}
+
+	public void canvasHost(int nextScreen)
+	{
+		canvas[nextScreen].SetActive(true);
+		canvas[lastScreen].SetActive(false);
+		lastScreen = nextScreen;
+	}
+}
