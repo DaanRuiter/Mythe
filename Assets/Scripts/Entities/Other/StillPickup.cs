9c3aa595778fd52cdd1e7ff47b56dc5e7296f24b
Niek Schoone <16789@ma-web.nl>
2015-02-17 15:52:12 +0100

    Fix BasePosition v2

---
+++ Assets/Scripts/Entities/Other/StillPickup.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StillPickup : FishPickup 
+{
+	private float _rotateRange = -0.1f;
+	
+	protected override void Start() 
+	{
+		base.Start();
+
+		speedMultiplier = 0.5f;
+
+		radiusY = 1;
+	}
+	
+	protected override void FixedUpdate () 
+	{
+		base.FixedUpdate();
+	}
+	
+	protected override void Movement()
+	{
+		direction = new Vector2(x, y);
+		
+		y = Mathf.Sin(mathSpeed) * radiusY;
+
+
+		transform.Rotate(0,0,_rotateRange);
+
+		if(transform.rotation.z >= 0.15)
+		{
+
+		}else if(transform.rotation.z <= )
+		{
+
+		}
+		Debug.Log(transform.rotation.z);
+
+		transform.position = basePosition + direction;
+	}
+}
