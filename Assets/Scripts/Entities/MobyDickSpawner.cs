84e338073c5607f41d24b7e3488bfab6bf035ef7
Niek Schoone <16789@ma-web.nl>
2015-03-10 13:25:08 +0100

    Moby Mechanics

--- Assets/Scripts/Entities/MobyDickSpawner.cs
+++ Assets/Scripts/Entities/MobyDickSpawner.cs
@@ -3,26 +3,43 @@ using System.Collections;
 
 public class MobyDickSpawner : MonoBehaviour 
 {
-
 	public GameObject moby;
+	private GameObject a;
 	public bool mobyHasSpawned;
+	public bool mobyHasDespawned;
 
 	void Start () 
 	{
 		mobyHasSpawned = false;
+		mobyHasDespawned = false;
 		float spawnTimeRange = Random.Range(2, 4);
 
 		Invoke("SpawnHim", spawnTimeRange);
+	}
 
-		Invoke("DespawnHim", spawnTimeRange + 4);
+	void Update()
+	{
+		if(a != null)
+		{
+			DespawnHim();
+		}else
+		{
+			mobyHasDespawned = false;
+			mobyHasSpawned = false;
+		}
 	}
 
 	void SpawnHim()
 	{
-		Instantiate(moby, new Vector3(29, -20, 0), Quaternion.identity);
+		a = Instantiate(moby, new Vector3(29, -15, 0), Quaternion.identity) as GameObject;
 		mobyHasSpawned = true;
 	}
 	void DespawnHim()
 	{
+		if(a.GetComponent<MobyDick>().despawning == true)
+		{
+			mobyHasSpawned = false;
+			mobyHasDespawned = true;
+		}
 	}
 }
