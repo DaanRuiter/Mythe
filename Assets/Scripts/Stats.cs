45d1a5f292c003da11020e145f74acc84fd4b3a8
Kramboja <koen5067@icloud.com>
2015-03-12 09:46:04 +0100

    HighscoreScriptsComplete + Menu improved

---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stats : MonoBehaviour {
+
+	void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+
+		//Haal hier alle waarden uit de save game op.
+	}
+}
--- PHP/HighScoreForm.php
+++ PHP/HighScoreForm.php
@@ -1,3 +1,17 @@
 <?php
 	include 'config.php';
+	
+	$username = $_REQUEST['username'];
+	$deviceId = $_REQUEST['deviceId'];
+	$score = $_REQUEST['score'];
+	$screenshot = $_REQUEST['fileToUpload'];
+		
+	$sql = "INSERT INTO Users (Username, DeviceId, HighScore)
+	VALUES ('$username', '$deviceId', '$score')";
+
+	if ($conn->query($sql) === TRUE) {
+	    echo "New record created successfully";
+	} else {
+	    echo "Error: " . $sql . "<br>" . $conn->error;
+	}
 ?>
