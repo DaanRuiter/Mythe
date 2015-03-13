b7c34974218725c84afb80e0ea719e5ef7cd5612
Kramboja <koen5067@icloud.com>
2015-03-13 11:31:57 +0100

    MenuImproved + code more OOP

---
+++ Assets/Scripts/UI/ButtonHandler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonHandler : MonoBehaviour {
+
+	public GameObject[] canvas;
+	private int lastScreen;
+
+	void Awake()
+	{
+		lastScreen = 0;
+	}
+
+	public void nextScene(string NextScene)
+	{
+		Application.LoadLevel(NextScene);
+	}
+
+	public void canvasHost(int nextScreen)
+	{
+		canvas[nextScreen].SetActive(true);
+		canvas[lastScreen].SetActive(false);
+		lastScreen = nextScreen;
+	}
+}
