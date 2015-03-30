a5ae6c0f9d17cbd2b8ea37558140ca4498287723
Daan Ruiter <daanruiter@live.nl>
2015-03-30 10:53:27 +0200

    victory condition & ui panel

--- Assets/Scripts/World/VictoryCondition.cs
+++ Assets/Scripts/World/VictoryCondition.cs
@@ -3,8 +3,33 @@ using System.Collections;
 
 public class VictoryCondition : MonoBehaviour {
 
-    public int fishPointsNeeded;
+    private int fishPointsNeeded;
     private int currentFishPoints;
+
+    public int percentageOfTotalNeeded;
+    public bool useCustomAmount;
+    public int customFishpointsNeeded;
+
+    private void Start()
+    {
+        if (!useCustomAmount)
+        {
+            GameObject[] fish = GameObject.FindGameObjectsWithTag("Interactable");
+            for (int i = 0; i < fish.Length; i++)
+            {
+                fishPointsNeeded += fish[i].GetComponent<PointsAndTypes>().GetPoints();
+            }
+            print(fishPointsNeeded);
+            float pointsNeeded = (float)fishPointsNeeded / 100;
+            print(pointsNeeded);
+            fishPointsNeeded = (int)(pointsNeeded * (float) percentageOfTotalNeeded);
+            print(fishPointsNeeded);
+        }
+        else
+        {
+            fishPointsNeeded = customFishpointsNeeded;
+        }
+    }
 
     public void AddFishPoints(int pointsToAdd)
     {
