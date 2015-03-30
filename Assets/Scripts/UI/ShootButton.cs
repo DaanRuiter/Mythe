e71e6347459dcfc5d90895c1b2be84aad66b1d01
Daan Ruiter <daanruiter@live.nl>
2015-03-30 08:43:48 +0200

    sync before publish

---
+++ Assets/Scripts/UI/ShootButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootButton : MonoBehaviour {
+
+    public HookShooter hook;
+
+    private void OnMouseEnter()
+    {
+        print("enter");
+    }
+
+    private void OnMouseDown()
+    {
+        hook.ShootHarpoon();
+    }
+}
---
+++ Assets/Scripts/World/VictoryCondition.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VictoryCondition : MonoBehaviour {
+
+    public int fishPointsNeeded;
+    private int currentFishPoints;
+
+    public void AddFishPoints(int pointsToAdd)
+    {
+        currentFishPoints += pointsToAdd;
+        if (hasWon)
+        {
+            //the player won the game.
+        }
+    }
+
+    private bool hasWon
+    {
+        get
+        {
+            if (fishPointsNeeded <= currentFishPoints)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
